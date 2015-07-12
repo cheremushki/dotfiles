bb049012ca7b86211e27f7ff9d4bd99558eadf88
Andrew Jones <andrew@skysedge.net>
2015-07-12 23:20:08 +0100

    - this works better in urxvt for ctrl-arrow key word jumping

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,7 +1,7 @@
 bindkey -v
 bindkey '^[[1;5D' backward-word
 bindkey '^[[1;5C' forward-word
-bindkey '^[OD' backward-word
-bindkey '^[OC' forward-word
+bindkey '^[Od' backward-word
+bindkey '^[Oc' forward-word
 bindkey '^[[3~' delete-char
 bindkey '^r' history-incremental-search-backward
