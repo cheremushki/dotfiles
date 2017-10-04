a65c3efd6de885063d6d3bbdf5834e61207496e8
Andrew Jones <andrew.jones@eu.clara.net>
2017-10-04 12:44:16 +0100

    Add a new alias: `dl`

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias reload!='. ~/.zshrc'
 alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
 alias vi="vim"
 alias host="host -N3"
+alias dl='curl --silent -w "Total:          %{time_total}\nDNS:            %{time_namelookup}\nConnect:        %{time_connect}\nPretransfer:    %{time_pretransfer}\nRedirect:       %{time_redirect}\nStart Transfer: %{time_starttransfer} \n" --output /dev/null '
