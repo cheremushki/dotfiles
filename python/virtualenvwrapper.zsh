0d52390f0d8462f49ec60b1215c010785a405574
Andrew Jones <4508105+cheremushki@users.noreply.github.com>
2018-09-13 20:24:31 +0100

    Set OSX-specific variables for virtualenvwrapper

--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,4 +1,8 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Documents/Projects/Code/python
-export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
-source /usr/local/bin/virtualenvwrapper_lazy.sh
+if [[ "$(uname -s)" == "Darwin" ]]
+then
+    export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
+    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+    source /usr/local/bin/virtualenvwrapper_lazy.sh
+fi
